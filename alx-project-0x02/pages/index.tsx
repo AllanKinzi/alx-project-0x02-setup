1e1577d56158f1871e4eebf494ab1fd2e269bb3b
Master <masterdev41@gmail.com>
2025-06-05 23:56:16 +0300

    Add Listing App files

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,19 @@
+import Layout from "@/components/layout/Layout";
+
+export default function Home() {
+  return (
+    <>
+      <section>
+        <p>Find your favorite place here!</p>
+        <p>The best prices for over 2 million properties worldwide.</p>
+      </section>
+      <section>
+        Filter Section
+      </section>
+
+      <section>
+        Listing Section
+      </section>
+    </>
+  );
+}

9d5830752df7d81d9ce586961f3e41b93597dc54
Master <masterdev41@gmail.com>
2025-06-06 11:48:19 +0300

    Correct links in header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -5,9 +5,9 @@ const Header = () => {
     <header className="bg-blue-600 text-white py-4 px-8 flex justify-between items-center">
         <Link href="/" className="text-white"><h1 className="text-2xl font-bold">Alx Listing App</h1> </Link>
       <nav className="flex gap-4">
-        <Link href={"/home"} className="text-white">Home</Link>
-        <Link href={"/about"} className="text-white">About</Link>
-        <Link href={"/posts"} className="text-white">Posts</Link>
+        <Link href="/home" className="text-white">Home</Link>
+        <Link href="/about" className="text-white">About</Link>
+        <Link href="/posts" className="text-white">Posts</Link>
       </nav>
     </header>
   );
